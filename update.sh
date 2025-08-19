38564777f914f648b63f82c06ae4914d2dd7a13c
MrQubo <jakub.jakub.nowak@gmail.com>
2025-08-20 01:20:10 +0200

    Switch shell to bashNonInteractive
    Switch shell interpreter to bashNonInteractive for smaller download
    size in CI.

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p bash -p nix-update
+#!nix-shell -p bashNonInteractive -p nix-update
 #!nix-shell -i bash
 
 exec nix-update -f update.nix --version=branch x86-manpages
